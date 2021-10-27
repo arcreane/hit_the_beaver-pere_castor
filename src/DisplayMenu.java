69745b7198a73cb84d72a3dad21606a68c99a696
thibault <thibault.blanchard@edu.itescia.fr>
2021-10-27 12:08:00 +0200

    DisplayMenu : Selection menu for the player - build main : Introduce function DisplayMenu

--- src/DisplayMenu.java
+++ src/DisplayMenu.java
@@ -4,20 +4,24 @@ public class DisplayMenu {
 
         static void displayMenu() {
 
-            System.out.println("Welcome to hit the beaver!!!!!!");
-            System.out.println("What do you want to do : ");
+            System.out.println("\t\t\t\t\t" + "Welcome to hit the beaver!!!!!!\n");
+            System.out.println("What do you want to do : \n");
             System.out.println("""
                     1. Play game
                     2. High Scores
                     3. Quit
                     """);
             System.out.println("choice : ");
+
+            /* Recover choice of the player*/
+
             Scanner scan = new Scanner(System.in);
             int playerChoiceMenu = scan.nextInt();
 
+            /*switch by playerChoiceMenu*/
             switch (playerChoiceMenu) {
                 case 1:
-                    /* To Fonction Difficulty choice*/
+                    /* To Function Difficulty choice*/
                     break;
                 case 2:
                     /* show high scores table*/
@@ -26,6 +30,7 @@ public class DisplayMenu {
                     System.out.println("See you later!!!!");
                     break;
                 default:
+                    /* if wrong selection of option return to Display Menu*/
                     System.out.println("Please enter 1, 2 or 3");
                     displayMenu();
 
