6563154f8122145f8445830f7c772bf0c09fba3d
angelrafael <generalpompeyo@hotmail.com>
2021-10-27 16:19:17 +0200

    Added DisplayMenu call and player input to recover coordinates

--- src/main.java
+++ src/main.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+
+        while (true) {
+
+
+
+            DisplayMenu.displayMenu();
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("""
+                    Where is the Beaver?!
+                    Enter x  and y coordinates : 
+                    """);
+            int x_coordinate = scanner.nextInt();
+            int y_coordinate = scanner.nextInt();
+
+            System.out.print("x : " + x_coordinate + ", y : " + y_coordinate);
+
+
+        }
+
+
     }
 }
